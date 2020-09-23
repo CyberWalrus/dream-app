4e340d066596c31eaf67b54295d6560b631dccce
Andrey Pakhomov <stonewalrusfan@gmail.com>
2020-09-23 22:19:45 +0300

    create app

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <StrictMode>
+    <div>Hello</div>
+  </StrictMode>,
+  document.getElementById('root')
+);
